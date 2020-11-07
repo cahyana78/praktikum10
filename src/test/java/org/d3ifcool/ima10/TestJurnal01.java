540197807759643daaddde0c99ba9d5980dce515
Cahyana <cahyana@tass.telkomuniversity.ac.id>
2020-11-07 21:27:04 +0700

    update Test

---
+++ src/test/java/org/d3ifcool/ima10/TestJurnal01.java
@@ -0,0 +1,26 @@
+package org.d3ifcool.ima10;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+public class TestJurnal01 {
+
+    private static final String[] OUTPUT = {
+            "1 4 9 16 25 36 49 64 81 100",
+    };
+
+    @Test
+    public void testJurnal() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+
+        Jurnal01.main(null);
+        assertEquals(OUTPUT[i] + "\n", bos.toString());
+        
+        System.setOut(originalOut);
+    }
+}
